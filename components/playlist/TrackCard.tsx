6d91626ef42a75906b918f90703a54d210d234f9
Jacob Castanon <jacastanon01@gmail.com>
2023-11-30 15:12:42 -0600

    added overlay to trackcard

--- components/playlist/TrackCard.tsx
+++ components/playlist/TrackCard.tsx
@@ -9,48 +9,57 @@ const TrackCard = ({ track }: { track: SpotifyApiResponseType['items'] }) => {
     <article className='w-full'>
       <div className='flex w-full items-center justify-between gap-4 p-4'>
         <div className='flex flex-col'>
-          <p className='mb-3 line-clamp-2 font-inter text-base md:text-3xl'>
-            {track.name}:{' '}
-          </p>
           {'artists' in track &&
-            track.artists?.map(({ name, id }: SpotifyArtistType) => (
-              <div className='text-primary hover:scale-y-125' key={id}>
-                <Link
-                  href={track.external_urls.spotify}
-                  target='_blank'
-                  className='font-roboto font-bold md:text-xl'
-                >
-                  {name}
-                </Link>
-              </div>
-            ))}
+            track.artists
+              ?.slice(0, 5)
+              .map(({ name, id }: SpotifyArtistType) => (
+                <div className='text-primary hover:scale-y-125' key={id}>
+                  <Link
+                    href={track.external_urls.spotify}
+                    target='_blank'
+                    className='font-roboto font-bold md:text-xl'
+                  >
+                    {name}
+                  </Link>
+                </div>
+              ))}
         </div>
-        {track && 'album' in track && track.album && (
-          <div className='relative h-32 w-32 flex-shrink-0 md:h-64 md:w-64'>
-            <Link
-              href={track.album.external_urls.spotify}
-              target='_blank'
-              className='absolute h-full w-full overflow-hidden rounded-lg'
-            >
-              <Image
-                // ? src={track.album.images[0].url ?? 'DEFAULT IMAGE'}
-                src={track.album.images[0].url}
-                alt={track.name}
-                fill
-                className='object-contain'
-                // width={track.album.images[0].width}
-                // height={track.album.images[0].height}
-              />
-            </Link>
-            {'preview_url' in track && track.preview_url && (
-              <figure className='flex h-full items-end py-2'>
-                <audio
-                  className='w-full bg-primary/70'
-                  controls
-                  src={track.preview_url}
-                />
-              </figure>
-            )}
+        {track && (
+          <div className='relative h-28 w-28 sm:h-64 sm:w-64'>
+            <div className='flex h-full w-full flex-col sm:gap-3'>
+              {'album' in track && track.album && (
+                <Link
+                  href={track.album.external_urls.spotify}
+                  target='_blank'
+                  className='relative h-full w-full overflow-hidden rounded-lg'
+                >
+                  <div className='bg-black/10 z-10 w-full py-2 backdrop-opacity-90 max-md:hidden md:absolute md:inline'>
+                    <p className='text-white z-30 line-clamp-2 px-4 font-inter text-2xl'>
+                      {track.name}
+                    </p>
+                  </div>
+                  <Image
+                    // ? src={track.album.images[0].url ?? 'DEFAULT IMAGE'}
+                    src={track.album.images[0].url}
+                    alt={track.name}
+                    fill
+                    className='object-fit z-0 md:hover:opacity-30'
+                    // width={track.album.images[0].width}
+                    // height={track.album.images[0].height}
+                  />
+                </Link>
+              )}
+              {'preview_url' in track && track.preview_url && (
+                <figure className='hidden w-full items-center py-2 sm:flex'>
+                  {/* <figcaption className='line-clamp-1'>{track.name}</figcaption> */}
+                  <audio
+                    className='rounded-xl bg-primary/70'
+                    controls
+                    src={track.preview_url}
+                  />
+                </figure>
+              )}
+            </div>
           </div>
         )}
       </div>
